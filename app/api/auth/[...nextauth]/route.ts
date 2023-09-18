28bd01714fb1863f1d5c7f119c1420e2cf3ad7b3
Anas Firly <anasfirly20@gmail.com>
2023-09-18 21:57:58 +0400

    ooving lib folder

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/lib/auth";
+import { authOptions } from "@/helpers/lib/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/lib/auth";
+import { authOptions } from "@/helpers/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 
