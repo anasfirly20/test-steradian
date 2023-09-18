131d7c6ed1f50c96432448a55ccf34cbdcb77e94
Anas Firly <anasfirly20@gmail.com>
2023-09-18 19:20:05 +0400

    adjusting

--- app/api/orders/route.tsx
+++ app/api/orders/route.tsx
@@ -5,16 +5,24 @@ import prisma from "@/prisma/client";
 // GET ALL ORDERS
 export async function GET(req: NextRequest) {
   const orders = await prisma.order.findMany({
-    select: {
-      id: true,
-      pickUpLoc: true,
-      dropOffLoc: true,
-      pickUpDate: true,
-      dropOffDate: true,
-      pickUpTime: true,
+    // select: {
+    //   id: true,
+    //   pickUpLoc: true,
+    //   dropOffLoc: true,
+    //   pickUpDate: true,
+    //   dropOffDate: true,
+    //   pickUpTime: true,
+    //   car: true,
+    //   user: true,
+    //   admin: true,
+    // },
+    include: {
       car: true,
       user: true,
       admin: true,
+    },
+    orderBy: {
+      pickUpDate: "asc",
     },
   });
   return NextResponse.json(orders);
