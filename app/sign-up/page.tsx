ffe497c76a5556710adfb358049f981dfcc7f777
Anas Firly <anasfirly20@gmail.com>
2023-09-18 09:57:27 +0400

    adding sign up form

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,15 +1,100 @@
 "use client";
 
-import { Input } from "@nextui-org/react";
+import { ChangeEvent, useState } from "react";
+
+// Components
+import CustomInput from "../components/CustomInput";
+import { Button } from "@nextui-org/react";
+
+const initialValues = {
+  username: "",
+  password: "",
+  email: "",
+  phone: "",
+  city: "",
+  zip: "",
+  message: "",
+  address: "",
+};
 
 export default function SignUpPage() {
+  const [data, setData] = useState(initialValues);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
   return (
     <article className="min-h-screen p-24">
+      <h1 className="font-bold text-center text-3xl">Sign Up</h1>
       <form
         // onSubmit={handleSubmit}
-        className="grid mt-10 w-[30%] mx-auto gap-y-5"
+        className="grid grid-cols-2 mt-10 w-[60%] mx-auto gap-x-5 gap-y-5"
       >
-        <Input label="Email" variant="bordered" className="text-black" />
+        <CustomInput
+          placeholder="Username"
+          type="text"
+          name="username"
+          value={data?.username}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="Password"
+          type="password"
+          name="password"
+          value={data?.password}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="Email"
+          type="email"
+          name="email"
+          value={data?.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="Phone Number"
+          type="text"
+          name="phone"
+          value={data?.phone}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="City"
+          type="text"
+          name="city"
+          value={data?.city}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="Zip Code"
+          type="number"
+          name="zip"
+          value={data?.zip}
+          onChange={(e) => handleChange(e)}
+        />
+        <CustomInput
+          placeholder="Address"
+          type="text"
+          name="address"
+          value={data?.address}
+          onChange={(e) => handleChange(e)}
+        />
+        <textarea
+          rows={4}
+          cols={50}
+          className="p-3 outline-none text-black rounded-lg w-full col-span-2"
+          name="message"
+          value={data?.message}
+          placeholder="Enter your message"
+          onChange={(e) => handleChange(e)}
+        />
+        <Button color="primary" className="text-lg font-semibold" type="submit">
+          Sign Up
+        </Button>
       </form>
     </article>
   );
